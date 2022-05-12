7467b5924fefbecd214e1a69e97f10d7f55a859f
andrew <statco19@gmail.com>
2022-05-13 00:34:40 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [message, setMessage] = useState([]);
+
+  useEffect(() => {
+    fetch("/hello")
+        .then((response) => {
+          return response.json();
+        })
+        .then(function (data) {
+          setMessage(data);
+        });
+    }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +31,9 @@ function App() {
         >
           Learn React
         </a>
+        <ul>
+          {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
+        </ul>
       </header>
     </div>
   );
